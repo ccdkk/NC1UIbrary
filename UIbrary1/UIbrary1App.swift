45532e09cacc214106c73c1699764400c9cff6a6
ccdkk <91456952+ccdkk@users.noreply.github.com>
2022-05-01 22:12:54 +0900

    Initial Commit

---
+++ UIbrary1/UIbrary1App.swift
@@ -0,0 +1,17 @@
+//
+//  UIbrary1App.swift
+//  UIbrary1
+//
+//  Created by 최동권 on 2022/05/01.
+//
+
+import SwiftUI
+
+@main
+struct UIbrary1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
